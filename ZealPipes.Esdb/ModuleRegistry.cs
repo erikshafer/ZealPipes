ff9ff51668c81b8ddc78fb83a17585d524c0b935
Erik Shafer <erik.shafer@gmail.com>
2024-06-02 23:29:14 -0500

    added an ESDB project with config module; bare minimum (WIP)

---
+++ ZealPipes.Esdb/ModuleRegistry.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ZealPipes.Esdb;
+
+public static class ModuleRegistry
+{
+    public static IServiceCollection AddEventStoreDb(IServiceCollection services, IConfiguration config)
+    {
+        // TODO: add to config of an app: { "EventStore": "esdb://localhost:2113?tls=false" }
+        const string connectionString = "esdb://admin:changeit@localhost:2113?tls=false&tlsVerifyCert=false";
+        services.AddEventStoreClient(new Uri(connectionString));
+        
+        return services;
+    }
+}
