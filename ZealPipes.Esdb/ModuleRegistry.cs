ceeffc1854273cd23fdbc54d01fa062380a9d6e9
Erik Shafer <erik.shafer@gmail.com>
2024-06-03 00:06:58 -0500

    using appsettings for esdb config, docker-compose setup

--- ZealPipes.Esdb/ModuleRegistry.cs
+++ ZealPipes.Esdb/ModuleRegistry.cs
@@ -7,9 +7,8 @@ public static class ModuleRegistry
 {
     public static IServiceCollection AddEventStoreDb(IServiceCollection services, IConfiguration config)
     {
-        // TODO: add to config of an app: { "EventStore": "esdb://localhost:2113?tls=false" }
-        const string connectionString = "esdb://admin:changeit@localhost:2113?tls=false&tlsVerifyCert=false";
-        services.AddEventStoreClient(new Uri(connectionString));
+        var connectionString = config["EventStore"];
+        services.AddEventStoreClient(new Uri(connectionString!));
         
         return services;
     }
